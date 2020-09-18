0fcfad2a0b1b95800fbb55ddcef6c13f94d2a8a4
hippeus <plachta.maciej@gmail.com>
2020-09-18 11:24:26 +1000

    feat(alpha_blog): article index page

--- alpha_blog/app/controllers/articles_controller.rb
+++ alpha_blog/app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     # byebug <- uncommenting this line will trigger debugger to break here
     @article = Article.find(params[:id])
   end
+
+  def index
+    @articles = Article.all
+  end
 end
