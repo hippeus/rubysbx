128ac69eefe2242dd3fa44acb1930f7baccded5e
hippeus <plachta.maciej@gmail.com>
2020-09-21 14:42:07 +1000

    feat(alpha_blog): disable field_error_proc from default Rails conf

--- alpha_blog/config/environment.rb
+++ alpha_blog/config/environment.rb
@@ -3,3 +3,9 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# disable default Rails behavior for input fields which may return errors
+
+ActionView::Base.field_error_proc = proc do |html_tag, _instance|
+  html_tag.html_safe
+end
