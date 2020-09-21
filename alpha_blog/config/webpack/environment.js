fb2b6aca47990571d91837e2f9c11121ed17646e
hippeus <plachta.maciej@gmail.com>
2020-09-21 12:10:09 +1000

    feat(alpha_blog): globally enable JS and Bootstrap/CSS styling in Rails app

--- alpha_blog/config/webpack/environment.js
+++ alpha_blog/config/webpack/environment.js
@@ -1,3 +1,16 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const env = require("@rails/webpacker/package/env");
 
-module.exports = environment
+// enabling js in Ruby app
+const webpack = require("webpack");
+
+environment.plugins.append(
+  "Provide",
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: "jquery",
+    Popper: ["popper.js", "default"],
+  })
+);
+
+module.exports = environment;
